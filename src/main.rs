21b32b0c6fa102f70a9089c8d09f341678b55224
vasilvas99 <ivanovvasil99@gmail.com>
2022-11-30 10:28:39 +0200

    Fix for larger terminals

--- src/main.rs
+++ src/main.rs
@@ -116,7 +116,7 @@ fn run_ui(
         Dialog::around(
             table
                 .with_name(table::TABLE_IDENTIFIER)
-                .min_size((200, 200)),
+                .min_size((400, 400)),
         )
         .title("Kanto-CM curses")
         .button("[S]tart", start_cb.clone())
